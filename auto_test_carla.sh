4ee01fdf5e0fa2d6b129001538b0abd9ccc83ef5
wuhanstudio <wuhanstudio@qq.com>
2023-11-23 10:51:39 +0000

    Add OC-SORT

--- auto_test_carla.sh
+++ auto_test_carla.sh
@@ -1,6 +1,7 @@
 for VIDEO in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
     python 2d-tracking-yolo-sort.py --video ${VIDEO} --dataset carla
+    python 2d-tracking-pcb-attack-oc-sort.py --video ${VIDEO} --dataset carla
     python 2d-tracking-pcb-attack-sort.py --video ${VIDEO} --dataset carla
 done
 
